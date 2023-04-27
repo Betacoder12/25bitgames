a7ca8ecd15f56550378cf0ba0ce9df8b9db838a0
Betacoder12 <131929462+Betacoder12@users.noreply.github.com>
2023-04-26 20:12:00 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+namespace flap {
+    /**
+     * make flappy:bit flap
+     */
+    //% blockId=arcadegames
+    //%block=flappybit $boolean
+
+
+
+ export function flap(bool: boolean){
+    if (bool) {
+     let flapping: number
+    flapping = 4
+        led.unplot(0, flapping)
+       flapping += -1
+        led.plot(0, flapping)
+    }
+     
+     
+     
+
+ }  
+}
